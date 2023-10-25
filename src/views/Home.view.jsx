133e4e131bba07fa0d7040ec3e81534e70d799c7
Sleepingoff <justjh30@gmail.com>
2023-10-25 10:28:15 +0900

    [ADD] 현재 위치를 기준으로 지도 생성하기
     - kakao Map API와 관련한 패키지 추가 설치

--- src/views/Home.view.jsx
+++ src/views/Home.view.jsx
@@ -1,41 +1,23 @@
 import React from "react";
 import PageTemplate from "../components/PageTemplate";
 import SearchBar from "../components/searchbar/searchbar";
-import { useQuery } from '@tanstack/react-query';
-import { maps } from "../config/api.config";
-import { useEffect } from "react";
 import useGeoLocation from "../hooks/useGeoLocation";
-import { useState } from "react";
+import { Map } from "react-kakao-maps-sdk";
 
 const HomePage = () => {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.async = true;
-        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_MAP_KEY}&autoload=false`;
-        document.head.appendChild(script);
-    
-        script.addEventListener("load", () => {
-            const {location} = useGeoLocation();
-            console.log(location);
-            window.kakao.maps.load(() => {
-            const container = document.getElementById("map");
-            const options = {
-              center: new window.kakao.maps.LatLng(location.latitude, location.longitude), // 초기 중심 좌표 (위도, 경도)
-              level: 3, // 지도 확대 레벨
-            };
-            new window.kakao.maps.Map(container, options);
-          });
-        });
-      }, []);
+  const {location} = useGeoLocation();
 
-    return(
-        <PageTemplate>
-            <SearchBar placeholder={"지하철역을 검색해주세요!"}/>
-            <div id="map">
-
-            </div>
-        </PageTemplate>
-    )
+  return(
+    <PageTemplate>
+      <SearchBar placeholder={"지하철역을 검색해주세요!"}/>
+      <Map 
+        center={{ lat: location.latitude, lng: location.longitude }}   
+        style={{ width: '100%', height: '600px' }} 
+        level={3}                                  
+      >
+      </Map>
+    </PageTemplate>
+  )
 }
 
 export default HomePage;
