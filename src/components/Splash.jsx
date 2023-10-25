ae7c1a79af972230e2effe7f69cb50cf5bc265db
kys0417 <yungsang1111@gmail.com>
2023-10-25 13:08:24 +0900

    [ADD] 스플래시 페이지 구현
    폰트, SNS 로그인 버튼 이미지 DISABLED시 #767676 처리 제외 구현

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo1 from '../../public/img/Logo1.svg';
 import Logo2 from '../../public/img/Logo2.svg';
+import Kakao from '../assets/kakao.svg';
+import Google from '../assets/google.svg';
+import Facebook from '../assets/facebook.svg';
+import Email from '../assets/email.svg';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import pageUrlConfig from '../config/pageUrlConfig';
 
 function Splash() {
   const SplashField = styled.div`
@@ -18,23 +24,40 @@ function Splash() {
     display: flex;
     flex-direction: column;
     gap: 10px;
-    padding: 40px 34px 70px 34px;
+    padding: 40px 34px 50px 34px;
     background: white;
     border-radius: 20px 20px 0 0;
   `;
   const SnsButton = styled.button`
-    padding: 14px 0 13px 0;
+    padding: 13px 0 11px 0;
     border-radius: 44px;
     border-width: 1px;
     border-style: solid;
     border-color: ${(props) => props.border};
+    background-image: url(${(props) => props.img});
+    background-repeat: no-repeat;
+    background-position: 14px 51%;
+  `;
+  const DisabledBtn = styled(SnsButton)`
+    cursor: default;
+    border-color: #767676;
+    color: #767676;
+    fill: #767676;
   `;
   const ButtonWrap = styled.div`
+    margin-top: 20px;
+    font-size: 12px;
+    color: #767676;
     display: flex;
     gap: 12px;
     justify-content: center;
     align-item: center;
   `;
+  const BottomBtn = styled.button`
+    font-size: 12px;
+    color: #767676;
+  `;
+  const navigate = useNavigate();
   const [showSplash, setShowSplash] = useState(true);
 
   useEffect(() => {
@@ -43,6 +66,14 @@ function Splash() {
     }, 1000);
   }, []);
 
+  const moveToLogin = () => {
+    navigate(pageUrlConfig.loginPage);
+  };
+
+  const moveToSingUp = () => {
+    navigate(pageUrlConfig.SignUpPage);
+  }
+
   return (
     <div>
       {showSplash ? ( // showSplash가 true일 때만 field_splash가 나타납니다.
@@ -50,14 +81,22 @@ function Splash() {
       ) : (
         <SplashField logo={Logo2} color={'#3c58c1'}>
           <SnsWrap>
-            <SnsButton border={'#3C58C1'}>이메일로 로그인</SnsButton>
-            <SnsButton border={'#F2C94C'}>카카오톡 계정으로 로그인</SnsButton>
-            <SnsButton border={'#767676'}>구글 계정으로 로그인</SnsButton>
-            <SnsButton border={'#2D9CDB'}>페이스북 계정으로 로그인</SnsButton>
+            <SnsButton border={'#3C58C1'} img={Email} onClick={moveToLogin}>
+              이메일로 로그인
+            </SnsButton>
+            <DisabledBtn border={'#F2C94C'} img={Kakao} disabled>
+              카카오톡 계정으로 로그인
+            </DisabledBtn>
+            <DisabledBtn border={'#767676'} img={Google}>
+              구글 계정으로 로그인
+            </DisabledBtn>
+            <DisabledBtn border={'#2D9CDB'} img={Facebook}>
+              페이스북 계정으로 로그인
+            </DisabledBtn>
             <ButtonWrap>
-              <button>아이디/비밀번호 찾기</button>
+              <BottomBtn>아이디/비밀번호 찾기</BottomBtn>
               <span>|</span>
-              <button>회원가입</button>
+              <BottomBtn onClick={moveToSingUp}>회원가입</BottomBtn>
             </ButtonWrap>
           </SnsWrap>
         </SplashField>
--- src/config/route.config.jsx
+++ src/config/route.config.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../views/Home.view';
 import ErrorPage from '../views/Error.view';
 import SplashPage from '../views/Splash.view';
+import LoginPage from '../views/Login.view';
+
 /** 라우트 등록하기
  * 1. routeConfig에 객체를 이용해서 path와 element 입력하기
  * 2. pageUrlConfig에 해당 path로 변수만들어서 link 이동 시 사용하기
@@ -9,6 +11,7 @@ import SplashPage from '../views/Splash.view';
 const routeConfig = [
   { path: `/main`, element: <HomePage />, errorElement: <ErrorPage /> },
   { path: `/`, element: <SplashPage />, errorElement: <ErrorPage /> },
+  { path: '/signIn', element: <LoginPage />, errorElement: <ErrorPage /> }
 ];
 const routers = createBrowserRouter(routeConfig);
 export default routers;
