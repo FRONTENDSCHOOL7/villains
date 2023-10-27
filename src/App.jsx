8937a3aeb296d6a72bfabefcee34facfb4c7a8c3
mjshin0425 <junis0425@gmail.com>
2023-10-27 17:31:23 +0900

    [FIX] 머지 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import routeConfig from './config/route.config';
 import { createHashRouter, createBrowserRouter, useRoutes } from 'react-router-dom';
 import { QueryClient, QueryCache, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { RouterProvider } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
