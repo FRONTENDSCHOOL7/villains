ddbb39dce18365c88fee14066495f34bb059df5d
Sleepingoff <justjh30@gmail.com>
2023-10-28 17:00:23 +0900

    [DELETE] 필요없는 파일 삭제

---
+++ src/views/Result.view.jsx
@@ -0,0 +1,12 @@
+import queryAtom from "../atoms/queryAtom";
+import PageTemplate from "../components/PageTemplate"
+
+const ResultPage = () => {
+    return(
+        <PageTemplate>
+
+        </PageTemplate>
+    )
+}
+
+export default ResultPage;
