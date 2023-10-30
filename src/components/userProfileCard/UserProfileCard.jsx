9e6ec42487c86c24b6d204792bcfde943506bdc3
mjshin0425 <junis0425@gmail.com>
2023-10-30 14:43:53 +0900

    [FIX] 머지 해결 완료

--- src/components/userProfileCard/UserProfileCard.jsx
+++ src/components/userProfileCard/UserProfileCard.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 //import './UserProfileCard.css';
 import profile_icon from '../Assets/UserImage.svg';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import pageUrlConfig from '../../config/pageUrlConfig';
 import PageTemplate from '../PageTemplate';
+import { SmallBtn } from '../Buttons';
 
 const UserProfileCard = () => {
+  const navigator = useNavigate();
+  const handleClickEdit = () => {
+    navigator(pageUrlConfig.profileEdit);
+  };
   return (
     <PageTemplate>
       <Title>프로필 페이지</Title>
@@ -22,27 +27,32 @@ const UserProfileCard = () => {
           <Followings>followings</Followings>
         </ProfileHeader>
 
-        <ProfileMain>
+        <ProfileContent>
           <UserName>나야나</UserName>
           <ProfileEmail>@villain_no1</ProfileEmail>
           <ProfileDsc>1호선 빌런 꿈나무</ProfileDsc>
-        </ProfileMain>
-
-        <div className="profile-correctBtn">
-          {/* <Link to="/user/edit">프로필 수정</Link> */}
-          <Link to={pageUrlConfig.profileEdit}>
-            <ProfileEditBtn>프로필 수정</ProfileEditBtn>
-            <DownSection>
-              <span>게시글</span>
-              <span>테마 목록</span>
-              <FeedCard>
-                <CardImg>Img</CardImg>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
-              </FeedCard>
-            </DownSection>
-          </Link>
-        </div>
+          {/* <Link to="/user/edit">프로필 수정</Link>  */}
+          <SmallBtn
+            background={'red'}
+            color={'black'}
+            cursor={'pointer'}
+            border={'black'}
+            text={'프로필 수정'}
+            onClick={handleClickEdit}
+            disabled={false}
+          >
+            프로필 수정
+          </SmallBtn>
+        </ProfileContent>
       </UpperSection>
+      {/* <DownSection>
+        <span>게시글</span>
+        <span>택배 목록</span>
+        <FeedCard>
+          <CardImg>Img</CardImg>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+        </FeedCard>
+      </DownSection> */}
     </PageTemplate>
   );
 };
@@ -66,6 +76,7 @@ const BackButton = styled.button`
 // 프로필 카드 상단부
 const UpperSection = styled.div`
   display: flex;
+  flex-direction: column;
   border: 0.5px solid blue;
   max-width: 400px;
   height: 200px;
@@ -73,20 +84,22 @@ const UpperSection = styled.div`
 
 const ProfileHeader = styled.div`
   border: 1px solid orange;
+  margin: 0 auto;
   max-width: 360px;
   height: 200px;
 `;
 
 const Followers = styled.div`
   display: inline-block;
+  vertical-align: middle;
 
   border: 0.5px solid #dbdbdb;
   max-width: 120px;
-  height: 120px;
 `;
 
 const ProfileImg = styled.div`
   display: inline-block;
+  vertical-align: middle;
 
   border: 0.5px solid #dbdbdb;
   max-width: 120px;
@@ -95,17 +108,16 @@ const ProfileImg = styled.div`
 
 const Followings = styled.div`
   display: inline-block;
+  vertical-align: middle;
 
   border: 0.5px solid #dbdbdb;
   max-width: 120px;
-  height: 120px;
 `;
 
 // 프로필 설명
-const ProfileMain = styled.div`
-  border: 1px solid orange;
-  max-width: 360px;
-  height: 200px;
+const ProfileContent = styled.div`
+  box-shadow: inset 0 0 10px gold;
+  text-align: center;
 `;
 
 const UserName = styled.p`
@@ -127,29 +139,33 @@ const ProfileDsc = styled.p`
   margin-bottom: 10px;
 `;
 
-const ProfileEditBtn = styled.button`
-  border: 1px solid #dbdbdb;
-  color: #767676;
-  border-radius: 4px;
-  width: 120px;
-  height: 34px;
-`;
+// const ProfileEditBtn = styled.button`
+//   border: 1px solid #dbdbdb;
+//   color: #767676;
+//   border-radius: 30px;
+//   width: 120px;
+//   height: 34px;
+// `;
 
 // 탭 포함하는 프로필 하단부
 const DownSection = styled.div`
-  border: 0.5px solid #dbdbdb;
+  border: 1px solid blue;
   max-width: 400px;
   height: 100vh;
 `;
 
-const FeedCard = styled.div`
-  border: 0.5px solid #dbdbdb;
-  max-width: 400px;
-  height: 700vh;
+const CardTab = styled.div`
+  border: 1px solid red;
 `;
 
-const CardImg = styled.div`
-  border: 0.5px solid #dbdbdb;
-  max-width: 300px;
-  height: 20vh;
-`;
+// const FeedCard = styled.div`
+//   border: 0.5px solid #dbdbdb;
+//   max-width: 400px;
+//   height: 700vh;
+// `;
+
+// const CardImg = styled.div`
+//   border: 0.5px solid #dbdbdb;
+//   max-width: 300px;
+//   height: 20vh;
+// `;
