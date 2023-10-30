44eb4d61e74957cf5e5c1b9b1f5a74d19406b063
Sleepingoff <justjh30@gmail.com>
2023-10-30 16:01:55 +0900

    [ADD] 지하철 시간표 가져오는 api 생성

---
+++ src/api/getSubTime.api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+/**
+ * @param START_INDEX: 페이지네이션 시작 인덱스
+ * @param END_INDEX: 페이지네이션 끝 인덱스
+ * @param STATION_CD: 지하철역 코드
+ * @param WEEK_TAG: 평일: 1, 토요일: 2, 휴일/일요일: 3
+ * @param INPUT_TAG: 상행/내선: 1, 하행/외선: 2
+ *
+ */
+const getSubTime = async ({ START_INDEX, END_INDEX, STATION_CD, WEEK_TAG, INPUT_TAG }) => {
+  return await axios.get(
+    `http://openAPI.seoul.go.kr:8088/${
+      import.meta.env.VITE_SEOUL_KEY
+    }/json/SearchSTNTimeTableByIDService/${START_INDEX}/${END_INDEX}/${STATION_CD}/${WEEK_TAG}/${INPUT_TAG}/`,
+  );
+};
+
+export default getSubTime;
