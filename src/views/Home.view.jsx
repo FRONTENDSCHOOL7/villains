236afc16719a42f5dff1d51345e13bf851ad5ac4
Sleepingoff <justjh30@gmail.com>
2023-10-31 17:27:44 +0900

    [MODIFY] 화면에 보여주는 데이터 교체

--- src/views/Home.view.jsx
+++ src/views/Home.view.jsx
@@ -5,65 +5,28 @@ import useGeoLocation from "../hooks/useGeoLocation";
 import { Map, MapMarker, CustomOverlayMap  } from "react-kakao-maps-sdk";
 import { useQuery } from '@tanstack/react-query'
 import contactQuery from "../api/getUserPost.api";
-import { useRecoilState, useRecoilValue } from "recoil";
-import queryAtom from "../atoms/queryAtom";
-import subOneAtom from "../atoms/subOneAtom";
 import getSubOneInfo from "../api/getSubOneInfo";
-import queryFocusAtom from "../atoms/queryFocusAtom";
-import getSubTime from "../api/getSubTime.api";
-import ListBox from '../components/searchbar/ListBox';
-import { useNavigate, useSearchParams } from "react-router-dom";
-import pageUrlConfig from "../config/pageUrlConfig";
+import subOneAtom from "../atoms/subOneAtom";
+import { useRecoilState } from "recoil";
+
 
 const HomePage = () => {
   const [subOneInfo, setSubOneInfo] = useRecoilState(subOneAtom);
-  const [list, setList] = useState([]);
-  const [isClickStation, setIsClickStation] = useState(false);
-  const [stationNum, setStationNum] = useState('');
-  const [showListBox, setShowListBox] = useState(false);
-  //전역에 저장한 검색어 꺼내오기
-  const query = useRecoilValue(queryAtom);
-  const [focus, setFocus] = useRecoilState(queryFocusAtom);
-
 
   useMemo(()=>{
     //도시철도 1호선 지하철역 정보 불러오기
     getSubOneInfo().then((data)=>{
       const dataList = data.data.SearchInfoBySubwayNameService.row;
       const rowInfo = [...dataList].filter(elem => elem.LINE_NUM === "01호선");
-
-      setSubOneInfo(rowInfo);
+      const newInfo = rowInfo.map((info, index)=>{
+        return info = {Query: info.STATION_NM, Id: info.STATION_CD};
+      })
+      setSubOneInfo(newInfo);
+      console.log(newInfo);
     });
   }, []);
   
-  const allList = subOneInfo;
-  useEffect(()=>{
-    if(query === "") setList([]);
-    else{
-      allList.map((data, index)=>{
-        if(data.STATION_NM.includes(query) && !list.find(elem => elem[0].includes(data.STATION_NM))){
-          setList([...list, [data.STATION_NM, data.STATION_CD]]);
-        }
-      })}
-  }, [query]);
-
-  useEffect(()=>{
-    if(focus) setShowListBox(true);
-  }, [focus])
   
-  const navigate = useNavigate();
-
-  useEffect(()=>{
-    if(isClickStation) {
-      navigate(`${pageUrlConfig.resultPage}/${stationNum}`);
-      
-    }
-  }, [isClickStation])
-  
-  const handleClickSub = (event) => {
-    setStationNum(event.currentTarget.dataset.etc);
-    setIsClickStation(true);
-  }
 
   const { location } = useGeoLocation();
   //사용자 위치 정보를 찾을 수 없을 때의 기본값이 필요합니다.
@@ -78,13 +41,9 @@ const HomePage = () => {
     if(data)    setPosts(data.data.post);
   }, [!isLoading && data]);
 
-  // const ListBox = lazy(()=>import( "../components/searchbar/ListBox"))
 
   return(
     <PageTemplate>
-        {showListBox ?
-          <ListBox list={list} onClick={handleClickSub}/>
-        : (
           <Map 
             center={{ lat: latitude, lng: longitude }}   
             style={{ width: '100%', height: '100vh' }} 
@@ -101,9 +60,6 @@ const HomePage = () => {
           })}
           <MapMarker position={{lat: latitude, lng: longitude}}></MapMarker>
           </Map>
-        )
-        
-      }
     </PageTemplate>
   )
 }
--- src/views/Result.view.jsx
+++ src/views/Result.view.jsx
@@ -1,68 +1,139 @@
-import { useLocation, useParams } from "react-router";
+import { useLocation, useNavigate, useParams } from "react-router";
 import getSubTime from "../api/getSubTime.api";
-import queryAtom from "../atoms/queryAtom";
+
 import PageTemplate from "../components/PageTemplate"
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+import holiday from '../database/2023-2024-holiday.json';
+import { useRecoilValue } from "recoil";
+import queryAtom from "../atoms/queryAtom";
+import queryFocusAtom from "../atoms/queryFocusAtom";
+import pageUrlConfig from "../config/pageUrlConfig";
+
 const ResultPage = () => {
-    const location = useLocation();
-    const index = 5;
-    const stationNum = location.pathname.split('/')[2];
+    const {stationId} = useParams();
+    const query = useRecoilValue(queryAtom);
+    const title = query;
+    const focus = useRecoilValue(queryFocusAtom);
+    
+    // const navigate = useNavigate();
+    // useEffect(()=>{
+    //     if(focus) navigate(`${pageUrlConfig.homePage}`);
+    // }, [focus])
+
+
+
     const [rowInfo, setRowInfo] = useState([]);
     const [reqCount, setReqCount] = useState(0);
     const [count, setCount] = useState(0);
-
+    const [day, setDay] = useState(1);
+    
     useEffect(()=>{
-        if(count === reqCount && count != 0){
-            const sortedInfo = rowInfo.sort();
-            console.log(sortedInfo);
-        }else{
-        getSubTime(index*count, index*(count+1)-1, stationNum, 1, 1)
+        setRowInfo([]);
+        setCount(0);
+    },[stationId])
+    
+    //TODO: 사용자 위치 정보 가져와서 시간에 맞게 시간표 조회하기
+    useEffect(()=>{
+        const userDate = new Date();
+        const today = userDate.getDay();// 0: 일요일 ~ 6: 토요일
+        const date = userDate.getDate();
+        const month = userDate.getMonth() + 1;
+        const year = userDate.getFullYear();
+        
+        switch(today){
+            case 6:
+                setDay(2);
+                break;
+            case 0:
+                setDay(3);
+                break;
+            default:
+                setDay(1);
+                break;
+        }
+
+        if(holiday.holiday.includes(`${year}-${month}-${date}`)){
+            setDay(3);
+        }
+    }, [])
+    
+
+    
+    //TODO(보류): 이미 검색한 기록이 있다면 리코일을 사용하여 불러오기
+    const index = 5;
+    useEffect(()=>{
+        rowInfo.sort();
+        getSubTime(index*count, index*(count+1)-1, stationId, day, 1)
         .then((res)=>{
             const access = res.data.SearchSTNTimeTableByIDService;
             const status = access.RESULT.CODE;
             if(status != 'INFO-000') throw access.RESULT.MESSAGE;
             const length = +access.list_total_count;
             setReqCount(Math.ceil(length/index));
-            setCount(prev=>prev+1);
             if(count<=reqCount){
                 access.row.map((info, index)=>{
-                    setRowInfo([...rowInfo, [info.ARRIVETIME, info.TRAIN_NO]]);
+                    rowInfo.push([info.ARRIVETIME,  info.TRAIN_NO, info.SUBWAYSNAME, info.SUBWAYENAME]);
                 })
             }
-            
-        }).catch((error)=>{
-            console.error(error);
-        });
-    }
-    }, [count])
-    
-
+            setCount(prev=>prev+1);
+    }).catch((error)=>{
+        console.error(error);
+    });
+    }, [count, stationId])
 
     return(
         <PageTemplate>
+            <StyledLinkBtn>{title}</StyledLinkBtn>
+            <ul>
             {
                 rowInfo.map((info, index)=>{
                     return (
-                    <div key={index}>
+                    <StyledList key={index}>
+                        <Time>{info[0]}</Time>
+                        <Station>{info[2]} ➡️ {info[3]}</Station>
                         <StyledTag aria-label="지하철 번호">{info[1]}</StyledTag>
-                        {info[0]}
-                    </div>
+                    </StyledList>
                     )
                 })
             }
+            </ul>
         </PageTemplate>
     )
 }
 
 export default ResultPage;
 
+const StyledLinkBtn = styled.button`
+    width: 100%;
+    height: 60px;
+    border-bottom: 1px solid #DBDBDB;
+    padding: 20px;
+    &:hover{
+        background-color: #B1BCE6;
+    }
+`;
+
 const StyledList = styled.li`
-    
+    display: flex;
+    flex-shrink: 0;
+    align-items: center;
+    padding: 20px;
+    border-bottom: 1px solid #DBDBDB;
+    font-size: 16px;
+    font-weight: 500;
 `
 
+const Time = styled.span`
+    margin-right: 8px;
+`
+const Station = styled.span`
+    margin-right: auto;
+    flex-grow: 1;
+`
 const StyledTag = styled.span`
+    text-algin: center;
     background-color: #3C58C1;
     color: white;
     border-radius: 9999px;
