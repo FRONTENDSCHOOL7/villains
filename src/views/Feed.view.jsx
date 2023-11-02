a1b2eccd5c96ff746cd5b47dff73412f87ff2259
gyull <kuri6233@gmail.com>
2023-11-02 23:57:44 +0900

    [MODIFY] 게시글 작성 플로팅 버튼 수정

--- src/views/Feed.view.jsx
+++ src/views/Feed.view.jsx
@@ -2,21 +2,31 @@ import PageTemplate from '../components/PageTemplate';
 import getPosts from '../api/getPosts.api';
 import styled from 'styled-components';
 import PostCard from '../components/feed/PostCard';
+import FloatingButton from '../components/FloatingButton.style';
+import write from '../assets/img/write.svg';
+import { useNavigate } from 'react-router';
+import pageUrlConfig from '../config/pageUrlConfig';
 
 const FeedPage = () => {
   const { posts, loading, error } = getPosts();
 
-  // if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error loading posts: {error.message}</div>;
+  const navigate = useNavigate();
+
+  const handleFeedWriteNav = () => {
+    navigate(pageUrlConfig.feedWritePage);
+  };
 
   return (
     <PageTemplate>
-      {posts&& <PostList>
-        {posts.map((post) => (
-          <PostCard post={post} key={post._id} />
-        ))}
-      </PostList>}
-  </PageTemplate>
+      {posts && (
+        <PostList>
+          {posts.map((post) => (
+            <PostCard post={post} key={post._id} />
+          ))}
+        </PostList>
+      )}
+      <FloatingButton img={write} onClick={handleFeedWriteNav} />
+    </PageTemplate>
   );
 };
 
