f1176a83f98ca54328c78f4aca3b48f0d59ec7a3
Sleepingoff <justjh30@gmail.com>
2023-11-03 11:47:58 +0900

    [CORS] 환경 변수 직접 사용하여 테스트

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@lib', replacement: './lib' },
-      { find: '@data', replacement: './data' },
-      { find: '@components', replacement: './components' },
-    ],
-  },
-  base: process.env.NODE_ENV === 'production' ? '/VILLAINS/' : '/',
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // Vite 설정
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: '@lib', replacement: './lib' },
+        { find: '@data', replacement: './data' },
+        { find: '@components', replacement: './components' },
+      ],
+    },
+    base: process.env.NODE_ENV === 'production' ? '/VILLAINS/' : '/',
+  };
 });
