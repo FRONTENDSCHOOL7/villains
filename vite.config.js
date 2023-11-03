06a4adb5834a4c10ee2387376ada232bdbf3915b
Sleepingoff <justjh30@gmail.com>
2023-11-03 15:02:36 +0900

    [MODIFY] 지도 API 변경

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,24 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, 'env');
+
+  const htmlPlugin = () => {
+    return {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(/%(.*?)%/g, function (match, p1) {
+          return env[p1];
+        });
+      },
+    };
+  };
   return {
     // Vite 설정
     // base: '/villains/',
     base: env.NODE_ENV === 'production' ? '/villains/' : '/',
-    plugins: [react()],
+    plugins: [react(), htmlPlugin()],
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
