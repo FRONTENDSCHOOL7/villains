75cbbd3509a17635abda07b087aafd1cf2430fee
Sleepingoff <justjh30@gmail.com>
2023-11-07 17:41:51 +0900

    [MODIFY] 프로필 페이지 수정

--- src/views/user/index.view.jsx
+++ src/views/user/index.view.jsx
@@ -1,10 +1,42 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useRouteLoaderData, useParams, useNavigate, useLocation } from "react-router";
+import NavMenu from '../../components/layout/NavMenu';
+import BackHeader from '../../components/layout/BackHeader';
+import DefaultBtn, { BasicStyle } from '../../components/GlobalButton';
+import BackArrow from '../../assets/img/icon-arrow-left.svg';
+import styled from 'styled-components';
+import Tanghulu from '../../components/Tanghulu';
+import pageUrlConfig from '../../config/pageUrlConfig';
 
 const ProfileIndexPage = () => {
-    return <>
-        <Outlet/>
-    </>   
+  const user = useRouteLoaderData('user');
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { accountname } = useParams();
+  
+  if(!accountname) navigate(`${pageUrlConfig.profilePage}/${user.accountname}`)
+
+  const handleClickBack = () => {
+    navigate(-1);
+  };
+  
+  return (
+    <>
+      <BackHeader>
+        <BackArrowBtn variant={'basic'} onClick={handleClickBack}>
+          <img src={BackArrow} alt="뒤로가기" />
+        </BackArrowBtn>
+        <Tanghulu />
+      </BackHeader>
+      <Outlet />
+      {pathname === pageUrlConfig.profilePage && <NavMenu />}
+    </>
+  ); 
 }
 
 export default ProfileIndexPage;
+
+const BackArrowBtn = styled.button`
+  ${BasicStyle}
+  margin-right: 8px;
+`;
