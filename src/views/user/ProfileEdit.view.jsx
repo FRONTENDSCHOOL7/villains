9d873ad2989afa57a3af8277b55b7ea2d9706809
kys0417 <92397578+kys0417@users.noreply.github.com>
2023-11-09 02:37:39 +0900

    [FIX] 프로필 수정, 내 게시글 불러오기 로직 수정 #17
    [FIX] 프로필 수정, 내 게시글 불러오기 로직 수정 #17

--- src/views/user/ProfileEdit.view.jsx
+++ src/views/user/ProfileEdit.view.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { useRouteLoaderData, useNavigate } from 'react-router-dom'; 
+import { useRouteLoaderData, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import PageTemplate from '../../components/layout/PageTemplate';import { useRecoilValue, useRecoilState } from 'recoil';
+import PageTemplate from '../../components/layout/PageTemplate';
+import { useRecoilValue, useRecoilState } from 'recoil';
 import basicProfile from '../../assets/img/basic-profile.svg';
 import { Input } from '../../components/default/Input.style';
 import profileAtom from '../../atoms/profileAtom';
@@ -12,6 +13,7 @@ import ResizingTextarea from '../../components/feed/ResizingTextarea';
 import triggerAtom from '../../atoms/tirggerAtom';
 import updateUser from '../../api/update/updateUser.api';
 import pageUrlConfig from '../../config/pageUrlConfig';
+import postImage from '../../api/post/postImage.api';
 // import imgIcon from `../assets/img/image-icon.svg`
 
 const ProfileEditPage = () => {
@@ -19,89 +21,116 @@ const ProfileEditPage = () => {
   const [myProfileInfo, setMyProfileInfo] = useRecoilState(profileAtom);
 
   const [name, setName] = useState(myProfileInfo.username);
-  const [intro, setIntro] = useState(myProfileInfo?.intro ?? "");
+  const [intro, setIntro] = useState(myProfileInfo?.intro ?? '');
+  const [image, setImage] = useState({
+    file: '',
+    url: myProfileInfo?.image ?? 'https://api.mandarin.weniv.co.kr/Ellipse.png',
+  });
 
   const [saveContents, setSaveContetns] = useRecoilState(triggerAtom);
 
-  const fileInputRef = useRef();
-  const [imagesData, setImagesData] = useState([]);
-  
   const handleChangeName = (event) => {
     setName(event.target.value);
-  }
+  };
   const handleChangeIntro = (event) => {
     setIntro(event.target.value);
-  }
+  };
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    const newInfo = { 
-      user: { 
-        "username": name, 
-        "intro": intro, 
-        "image": imagesData
-      } 
+  useEffect(async () => {
+    const urls = image.file ? await postImage(image.file) : image.url;
+    const newInfo = {
+      user: {
+        username: name,
+        intro: intro,
+        image: urls,
+      },
     };
-    if(saveContents){
-
+    console.log(newInfo);
+    if (saveContents) {
       updateUser(newInfo, user.token)
-      .then((result)=>{
-        navigate(`${pageUrlConfig.profilePage}/${user.accountname}`, {state: newInfo})
-      })
-      .catch((error)=>{
-        alert('이미 사용 중인 이름입니다');
-        console.error(error);
-      })
-    setSaveContetns(false);
-    
-  }
+        .then((result) => {
+          navigate(`${pageUrlConfig.profilePage}/${user.accountname}`, { state: newInfo });
+        })
+        .catch((error) => {
+          alert('이미 사용 중인 이름입니다');
+          console.error(error);
+        });
+      setSaveContetns(false);
+    }
   }, [saveContents]);
 
-  const handleImageChange = async (event) => {
-    const files = Array.from(event.target.files);
+  let inputRef;
 
-    if (imagesData.length + files.length > 3) {
-      alert('최대 3개의 이미지만 업로드할 수 있습니다.');
-      return;
+  const handleSaveImage = async (e) => {
+    const fileReader = new FileReader();
+    if (e.target.files[0]) {
+      fileReader.readAsDataURL(e.target.files[0]);
     }
-
-    try {
-      const uploadedImageUrls = await postImages(files);
-      setImagesData((prevData) => [...prevData, ...uploadedImageUrls]);
-    } catch (error) {
-      console.error('이미지 업로드에 실패했습니다:', error);
-    }
+    fileReader.onload = () => {
+      setImage({
+        file: e.target.files[0],
+        url: fileReader.result,
+      });
+    };
   };
 
-  const triggerFileInput = () => {
-    fileInputRef.current.click();
+  // const handleImageChange = async (event) => {
+  //   try {
+  //     const urls = image.file ? await postImage(image.file) : image.url;
+  //     console.log(uploadedImageUrls);
+  //     setImagesData((prevData) => [...prevData, ...uploadedImageUrls]);
+  //   } catch (error) {
+  //     console.error('이미지 업로드에 실패했습니다:', error);
+  //   }
+  // };
+
+  const triggerFileInput = (e) => {
+    e.preventDefault();
+    inputRef.click();
   };
   return (
     <PageTemplate>
       <ImgSection>
-        <ProfileImg src={basicProfile} alt="프로필 이미지" />
+        <ProfileImg
+          src={
+            image.url === 'http://146.56.183.55:5050/Ellipse.png'
+              ? 'https://api.mandarin.weniv.co.kr/Ellipse.png'
+              : image.url
+          }
+          alt="프로필 이미지"
+        />
         <InsertImageBtn htmlFor="file">
-          <FloatingButton img={imageIcon} type="button" onClick={triggerFileInput} style={{
-            top: `180px`, bottom: `auto`, right: `calc(50% - 50px)`
-          }} />
+          <FloatingButton
+            img={imageIcon}
+            type="button"
+            onClick={triggerFileInput}
+            style={{
+              top: `180px`,
+              bottom: `auto`,
+              right: `calc(50% - 50px)`,
+            }}
+          />
         </InsertImageBtn>
         <InputFile
-          ref={fileInputRef}
+          ref={(refParam) => (inputRef = refParam)}
           type="file"
           id="file"
           accept="image/*"
           multiple
-          onChange={handleImageChange}
+          onChange={handleSaveImage}
         />
       </ImgSection>
 
       <InputSection>
-        <Label>사용자 이름
-        <Input placeholder="2~10자 이내" value={name} onChange={handleChangeName}/>
+        <Label>
+          사용자 이름
+          <Input placeholder="2~10자 이내" value={name} onChange={handleChangeName} />
         </Label>
 
-        <Label>소개
-        <ResizingTextarea placeholder="자기소개" value={intro} onChange={handleChangeIntro} />
+        <Label>
+          소개
+          <ResizingTextarea placeholder="자기소개" value={intro} onChange={handleChangeIntro} />
         </Label>
       </InputSection>
     </PageTemplate>
@@ -125,16 +154,17 @@ const InputSection = styled.section`
 `;
 
 const ProfileImg = styled.img`
-  max-width: 100%;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
   flex-shrink: 0;
   flex-basis: calc(100% / 3);
   margin-top: 30px;
   margin-bottom: 30px;
+  object-fit: cover;
 `;
 
 const InsertImageBtn = styled.label`
-  width: 50px;
-  height: 50px;
   z-index: 10;
 `;
 
@@ -145,9 +175,10 @@ const InputFile = styled.input`
 const Label = styled.label`
   font-size: ${theme.fontSize.caption};
   color: ${theme.color.grey};
-  &, input {
+  &,
+  input {
     display: block;
-    padding: 10px 0 ;
+    padding: 10px 0;
     width: 100%;
   }
 `;
