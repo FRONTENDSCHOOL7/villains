33071ad7990d94e3958353ea870eaacaa351676f
Sleepingoff <justjh30@gmail.com>
2023-11-27 20:52:41 +0900

    [ADD] 스토리북 추가

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,47 @@
+import DefaultBtn from '../components/button/GlobalButton';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Comp/Button',
+  component: DefaultBtn,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    variant: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'basic'],
+      description: 'The value of the variant',
+    },
+    disabled: {
+      control: { type: 'boolean' },
+    },
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {
+    variant: 'primary',
+    children: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    variant: 'secondary',
+    children: 'Button',
+  },
+};
+
+export const Basic = {
+  args: {
+    variant: 'basic',
+    children: 'Button',
+  },
+};
