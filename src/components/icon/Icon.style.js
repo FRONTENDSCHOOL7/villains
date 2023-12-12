449562208675abb4e735e678c4bb1770593a8879
Sleepingoff <justjh30@gmail.com>
2023-12-12 11:22:48 +0900

    [ADD] icon 컴포넌트 일괄 생성

---
+++ src/components/icon/Icon.style.js
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components';
+
+const Size = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 1px solid black;
+`;
+
+export default Size;
