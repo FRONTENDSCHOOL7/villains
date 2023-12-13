ab53308d471343debc6dfff3e3d35941b18c63cb
Sleepingoff <justjh30@gmail.com>
2023-12-13 15:46:53 +0900

    [ADD] 이미지 컴포넌트 스토리북 구현

---
+++ src/components/card/ImageItem.jsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+const DefaultImage = () => {
+    return(
+        <ImagePreviewCont>
+            <PreviewSpan>
+            오른쪽 하단 버튼을 눌러
+            <br />
+            이미지를 추가해보세요.
+            </PreviewSpan>
+        </ImagePreviewCont>
+    )
+}
+
+const SingleImage = ({url, onClick}) => {
+    return(
+        <ImageWrapper>
+        <StyledImage src={url} alt="" />
+        <DeleteButton aria-label="이미지 삭제 버튼" type="button" onClick={() => onClick(idx)} />
+      </ImageWrapper>
+    )
+}
+
+const MultiImage = ({imageUrls, onClick}) => {
+    return (
+        <Swiper spaceBetween={10} slidesPerView={1.2} pagination={{ clickable: true }}>
+        {imageUrls.map((url, idx) => (
+            <SwiperSlide key={idx}>
+                <SingleImage url={url} onClick={onClick} />
+            </SwiperSlide>
+        ))}
+        </Swiper>
+    )
+}
+
+const Image = {
+    Default: DefaultImage,
+    Single: SingleImage,
+    Multi: MultiImage
+}
+
+export default Image;
+
+const ImagePreviewCont = styled.div`
+  width: 100%;
+  height: 204px;
+  border-radius: 10px;
+  border: 0.5px solid #dbdbdb;
+  background: #f4f4f4 ;
+  margin-bottom: 30px;
+
+  color: #c4c4c4;
+  text-align: center;
+  font-size: 12px;
+  line-height: 15px;
+`;
+
+const PreviewSpan = styled.span`
+  position: relative;
+  top: 124px;
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 204px;
+  margin-bottom: 28px;
+  object-fit: fill;
+  border-radius: 10px;
+  border: 0.5px solid #dbdbdb;
+`;
+
+const DeleteButton = styled.button`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  width: 22px;
+  height: 22px;
+`;
